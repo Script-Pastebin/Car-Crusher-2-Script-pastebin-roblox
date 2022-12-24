79202e0af27ba6ab663b7c79cd8ab6f610384c2b
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-24 06:59:09 +0100

    commit zero

---
+++ Car Crusher 2 Script pastebin roblox Car Crusher 2 GUI .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+-- // Configuration: \\ --   shared.settings = { ['DarkMode'] = true; -- Dark mode on the script itself. ['AntiAfk'] = true; -- Enable anti-afk ['CloseBind'] = Enum.KeyCode.Semicolon; -- Set this to your preferred key to close the menu. }   -- // Loader: \\ --   loadstring(game:HttpGet('https://pastebin.com/raw/T7ERd3e8'))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script for Auto Farm and it also has Teleports!
+						
+]]
